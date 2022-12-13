3a33e500b8e6df8a8f12291b7265c60c743697a2
unknown <srikanth.pullavarti@bedrockapps.org>
2022-12-13 20:13:45 +0530

    details page change

--- src/pages/Dashboard/DirectContact.jsx
+++ src/pages/Dashboard/DirectContact.jsx
@@ -11,11 +11,11 @@ import { useTranslation } from "react-i18next";
 import { makeStyles } from "@mui/styles";
 import { GlobalState } from "../../Context/Context";
 import ContactPageIcon from "@mui/icons-material/ContactPage";
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import logo from "../../Images/Bedrock Rock .png";
 import { useEffect } from "react";
 import { getContactsList } from "../../services/request";
-
+import { Box } from "@mui/system";
 
 const useStyle = makeStyles(() => ({
   titleText: {
@@ -48,16 +48,24 @@ const DirectContact = (props) => {
   const classes = useStyle();
   const { t } = useTranslation();
   // const { list } = props;
-  const { selectedChat, setSelectedChat, setPopen, openUserForm, setOpenUserForm, list, setList } = useContext(GlobalState);
+  const {
+    selectedChat,
+    setSelectedChat,
+    setPopen,
+    openUserForm,
+    setOpenUserForm,
+    list,
+    setList,
+  } = useContext(GlobalState);
 
   useEffect(() => {
     let ownerId = localStorage.getItem("userId");
-    let role = localStorage.getItem("role")
-    if(role === "Owner"){
+    let role = localStorage.getItem("role");
+    if (role === "Owner") {
       getContactsList(ownerId, "Contractor")
         .then((res) => {
           if (res.status === 200) {
-            console.log(res)
+            console.log(res);
             if (res?.data?.length > 0) {
               setList([...res?.data]);
             } else {
@@ -70,87 +78,142 @@ const DirectContact = (props) => {
           console.log(errorObj);
         });
     }
-  }, [])
+  }, []);
   return (
     <>
-    {localStorage.getItem("role") === "Owner" ?
-    <>
-      <Grid container spacing={1}>
-        <Grid item xs={11}>
-          <Typography className={classes.titleText}>
-            {t("direct_contacts")}
-          </Typography>
-          </Grid>
-          <Grid item xs={1}>
-            <IconButton
-              color="primary"
-              size="small"
-              onClick={() => setOpenUserForm(true)}
+      {localStorage.getItem("role") === "Owner" ? (
+        <>
+          <Grid container spacing={1}>
+            <Grid item xs={11}>
+              <Typography className={classes.titleText}>
+                {t("direct_contacts")}
+              </Typography>
+            </Grid>
+            <Grid item xs={1}>
+              <IconButton
+                color="primary"
+                size="small"
+                onClick={() => setOpenUserForm(true)}
+              >
+                <Tooltip title={"create contact"}>
+                  <PersonAddIcon />
+                </Tooltip>
+              </IconButton>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sx={{ height: "55vh", overflowY: "auto", paddingRight: "5px" }}
             >
-              <Tooltip title={"create contact"}>
-                <PersonAddIcon />
-              </Tooltip>
-            </IconButton>
+              {list?.length > 0 ? (
+                list?.map((item, i) => (
+                  <List
+                    disablePadding
+                    key={item?.firstName + i}
+                    sx={{
+                      width: "100%",
+                      bgcolor: "#e6e5ea",
+                      borderRadius: "5px",
+                      marginBottom: "10px",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => {
+                      setPopen(true);
+                      setSelectedChat(item);
+                    }}
+                  >
+                    <ListItem alignItems="center">
+                      <ListItemAvatar sx={{ margin: "0px" }}>
+                        <Avatar
+                          alt=""
+                          src={Profile}
+                          className={classes.avatar}
+                        />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={
+                          <>
+                            <Typography
+                              className={classes.nameText}
+                            >{`${item?.firstName} ${item?.lastName} | ${item?.role}`}</Typography>
+                          </>
+                        }
+                        secondary={
+                          <>
+                            <Typography
+                              className={classes.emailText}
+                            >{`${item?.email} | Ph No: ${item?.phoneNumber}`}</Typography>
+                          </>
+                        }
+                      ></ListItemText>
+                    </ListItem>
+                  </List>
+                ))
+              ) : (
+                <Grid
+                  container
+                  spacing={1}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    textAlign: "center",
+                  }}
+                >
+                  <Grid item xs={8} sx={{ mt: 5 }}>
+                    <Avatar
+                      alt=""
+                      src={logo}
+                      sx={{
+                        height: "200px",
+                        width: "200px",
+                        borderRadius: "0px",
+                      }}
+                    />
+                  </Grid>
+                  <Grid
+                    item
+                    xs={8}
+                    sx={{ mt: 1, fontSize: "20px", fontWeight: "700" }}
+                  >
+                    Welcome to BedRock!
+                  </Grid>
+                </Grid>
+              )}
+            </Grid>
           </Grid>
-        <Grid
-          item
-          xs={12}
-          sx={{ height: "55vh", overflowY: "auto", paddingRight: "5px" }}
-        >
-          {list?.map((item, i) => (
-            <List
-              disablePadding
-              key={item?.firstName + i}
-              sx={{
-                width: "100%",
-                bgcolor: "#e6e5ea",
-                borderRadius: "5px",
-                marginBottom: "10px",
-                cursor:"pointer"
-              }}
-              onClick={() => {
-                setPopen(true);
-                setSelectedChat(item);
-              }}
+        </>
+      ) : (
+        <>
+          <Grid
+            container
+            spacing={1}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              textAlign: "center",
+            }}
+          >
+            <Grid item xs={8} sx={{ mt: 5 }}>
+              <Avatar
+                alt=""
+                src={logo}
+                sx={{ height: "200px", width: "200px", borderRadius: "0px" }}
+              />
+            </Grid>
+            <Grid
+              item
+              xs={8}
+              sx={{ mt: 1, fontSize: "20px", fontWeight: "700" }}
             >
-              <ListItem alignItems="center">
-                <ListItemAvatar sx={{ margin: "0px" }}>
-                  <Avatar alt="" src={Profile} className={classes.avatar} />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <>
-                      <Typography
-                        className={classes.nameText}
-                      >{`${item?.firstName} ${item?.lastName} | ${item?.role}`}</Typography>
-                    </>
-                  }
-                  secondary={
-                    <>
-                      <Typography
-                        className={classes.emailText}
-                      >{`${item?.email} | Ph No: ${item?.phoneNumber}`}</Typography>
-                    </>
-                  }
-                ></ListItemText>
-              </ListItem>
-            </List>
-          ))}
-        </Grid>
-      </Grid>
-    </>
-    :
-    <>
-      <Grid container spacing={1} sx={{display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", textAlign:"center"}}>
-        <Grid item xs={8} sx={{mt:5}}>
-          <Avatar alt="" src={logo} sx={{height:"200px", width:"200px", borderRadius:"0px"}}/>
-        </Grid>
-        <Grid item xs={8} sx={{mt:1, fontSize:"20px", fontWeight:"700"}}>
-          Welcome to BedRock!
-        </Grid>
-      </Grid>
-    </>
-    }
+              Welcome to BedRock!
+            </Grid>
+          </Grid>
+        </>
+      )}
     </>
   );
 };
